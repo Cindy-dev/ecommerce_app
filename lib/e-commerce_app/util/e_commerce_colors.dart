bb7fb21ce26459e71e5f3d2d65980311494f1c03
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-03-08 12:46:58 +0100

    adding gray color to the color class

--- lib/e-commerce_app/util/e_commerce_colors.dart
+++ lib/e-commerce_app/util/e_commerce_colors.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 class EcommerceColors{
   static const gray = Color(0xffC8C8CB);
+  static const grayText = Color(0xff939393);
   static const black = Color(0xff393F42);
   static const border1 = Color(0xffF0F2F1);
 }
