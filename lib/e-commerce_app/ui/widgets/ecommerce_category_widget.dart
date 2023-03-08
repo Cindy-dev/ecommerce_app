aa3d09fe52ce99f464ce144e23e43eb1dc78208b
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-03-08 12:55:48 +0100

    category tab design implemented

--- lib/e-commerce_app/ui/widgets/ecommerce_category_widget.dart
+++ lib/e-commerce_app/ui/widgets/ecommerce_category_widget.dart
@@ -8,7 +8,7 @@ class EcommerceCategoryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+      padding: const EdgeInsets.fromLTRB(20,6,20,17),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -20,23 +20,46 @@ class EcommerceCategoryWidget extends StatelessWidget {
                 color: EcommerceColors.black,
                 fontSize: 15),
           ),
-          const SizedBox(height: 7,),
-          Row(children: [
-            Column(children: [
-              Image.asset("assets/e_commerce_app/e_commerce_image/clothes.png", scale: 3,),
-              const SizedBox(height: 6),
-              const Text(
-                "Apparel",
-                style: TextStyle(
-                    fontFamily: "Inter",
-                    fontWeight: FontWeight.w400,
-                    color: EcommerceColors.black,
-                    fontSize: 12),
-              ),
-            ],)
-          ],)
+          const SizedBox(
+            height: 7,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              catTile("assets/e_commerce_app/e_commerce_image/clothes.png",
+                  "Apparel"),
+              catTile("assets/e_commerce_app/e_commerce_image/stationery.png",
+                  "School"),
+              catTile("assets/e_commerce_app/e_commerce_image/stationery.png",
+                  "Sports"),
+              catTile("assets/e_commerce_app/e_commerce_image/kitchen.png",
+                  "Electronic"),
+              catTile("assets/e_commerce_app/e_commerce_image/nav.png", "All")
+            ],
+          )
         ],
       ),
+    );
+  }
+
+  //horizontal tile of different categories in the app
+  Widget catTile(String image, String title) {
+    return Column(
+      children: [
+        Image.asset(
+          image,
+          scale: 3,
+        ),
+        const SizedBox(height: 6),
+        Text(
+          title,
+          style: const TextStyle(
+              fontFamily: "Inter",
+              fontWeight: FontWeight.w400,
+              color: EcommerceColors.grayText,
+              fontSize: 12),
+        ),
+      ],
     );
   }
 }
