4a3058660ee2ad6746740ba6635fafa7d9a596ea
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-03-09 14:46:30 +0100

    design finishes of the search page

--- lib/e-commerce_app/ui/views/e_commerce_search_page.dart
+++ lib/e-commerce_app/ui/views/e_commerce_search_page.dart
@@ -15,12 +15,65 @@ class ECSearchPage extends StatelessWidget {
       padding: EdgeInsets.only(top: height / 16, left: 20, right: 20),
       child: Column(
         children: [
-          ECSearchHeader(),
+          const ECSearchHeader(),
           const SizedBox(height: 14),
           const Divider(
             color: EcommerceColors.gray,
           ),
           const SizedBox(height: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "Last search",
+                style: TextStyle(
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w500,
+                    color: EcommerceColors.black,
+                    fontSize: 16),
+              ),
+              Text(
+                "Clear all",
+                style: TextStyle(
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w500,
+                    color: EcommerceColors.red,
+                    fontSize: 12),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: const [
+                   Icon(
+                    Icons.access_time,
+                    color: EcommerceColors.gray,
+                  ),
+                  SizedBox(width: 12,),
+                  Text(
+                    "Iphone 12 pro max",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w400,
+                        color: EcommerceColors.black,
+                        fontSize: 14),
+                  ),
+                ],
+              ),
+
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.close,
+                    color: EcommerceColors.gray,
+                  ))
+            ],
+          ),
         ],
       ),
     ));
